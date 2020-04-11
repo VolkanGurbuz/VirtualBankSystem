92c82611ee233506577dce5a522a01008504e9fe
volkangurbuz <volkangurbuz16@gmail.com>
2020-04-11 16:56:36 +0200

    added isValid method with getting the attribute

--- Identity-Verification-Service-Rest-API/src/main/java/com/developer/guys/Application.java
+++ Identity-Verification-Service-Rest-API/src/main/java/com/developer/guys/Application.java
@@ -5,12 +5,17 @@ import com.developer.guys.Entities.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.xml.soap.SOAPBody;
+
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        Util.sendMessage(new Person(
-                "21892064260","SEZER","YILDIRIM","1993"
-        ));
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+
+    Util util = new Util();
+
+    SOAPBody soapBody = util.sendMessage(new Person("21892064260", "SEZER", "YILDIRIM", "1993"));
+
+    System.out.println("isValid: " + util.isValid(soapBody));
+  }
 }
