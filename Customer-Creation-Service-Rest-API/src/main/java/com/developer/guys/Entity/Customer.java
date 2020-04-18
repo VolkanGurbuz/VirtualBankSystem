38dbaba90abb8f11443dbdbee114f9c92a68dfdf
Sezer YILDIRIM <sezer.yildirim@live.com>
2020-04-18 21:07:51 +0300

    Adding Customer Creation Service Background and MongoDB Connection

---
+++ Customer-Creation-Service-Rest-API/src/main/java/com/developer/guys/Entity/Customer.java
@@ -0,0 +1,19 @@
+package com.developer.guys.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashMap;
+
+@Getter
+@Setter
+@Document
+public class Customer {
+    @Id
+    private String id;
+    private String adi;
+    private String soyadi;
+    private HashMap ozellikleri;
+}
